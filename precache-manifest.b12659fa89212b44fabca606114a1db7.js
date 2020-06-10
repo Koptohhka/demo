47da18941fdad83270dc993ff14903fdbffb1862
Pan Artur <artur.pankov.16@list.ru>
2020-06-10 19:26:34 +0300

    test

---
+++ precache-manifest.b12659fa89212b44fabca606114a1db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f24645315673ac5bebd4dce64636b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "debd1a59d4e08a5e1492",
+    "url": "/static/css/main.e56f659c.chunk.css"
+  },
+  {
+    "revision": "3c72c239807dd78e1d6e",
+    "url": "/static/js/2.bcc16bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcc16bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "debd1a59d4e08a5e1492",
+    "url": "/static/js/main.972f4ad6.chunk.js"
+  },
+  {
+    "revision": "239beb865cf95fd35ea4",
+    "url": "/static/js/runtime-main.4a5b887e.js"
+  },
+  {
+    "revision": "66fe947f57ff48b455587c06373aa2c0",
+    "url": "/static/media/img-01.66fe947f.png"
+  }
+]);
