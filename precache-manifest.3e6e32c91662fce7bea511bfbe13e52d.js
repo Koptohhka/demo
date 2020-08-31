212a6afddef6549db27e5865dfd495f1fc62e973
Pan Artur <artur.pankov.16@list.ru>
2020-08-31 22:16:49 +0300

    commit

---
+++ precache-manifest.3e6e32c91662fce7bea511bfbe13e52d.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe3cdb11dee7ec175ff0c5372b73225",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f887ae2cb271a0afec01",
+    "url": "./static/css/main.4160980d.chunk.css"
+  },
+  {
+    "revision": "3be2a312c6cd97c662be",
+    "url": "./static/js/2.7d5a834f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.7d5a834f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f887ae2cb271a0afec01",
+    "url": "./static/js/main.0471b1e7.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  },
+  {
+    "revision": "fd0ecef08dc2ad951a18f4e342796706",
+    "url": "./static/media/BishopBlack.fd0ecef0.svg"
+  },
+  {
+    "revision": "e69442641c9b88508504f5f650bd4525",
+    "url": "./static/media/BishopWhite.e6944264.svg"
+  },
+  {
+    "revision": "e0b57a1fd1ae2e2c5b487a56202d220f",
+    "url": "./static/media/Comp.e0b57a1f.png"
+  },
+  {
+    "revision": "893f9ab561430cb1420b06c8060797cc",
+    "url": "./static/media/KingBlack.893f9ab5.svg"
+  },
+  {
+    "revision": "9ed0b088f45f2ba11a33d844a3bd5d64",
+    "url": "./static/media/KingWhite.9ed0b088.svg"
+  },
+  {
+    "revision": "80432bc48eaa1c85bab8fa986043cc17",
+    "url": "./static/media/KnightBlack.80432bc4.svg"
+  },
+  {
+    "revision": "7982c32abaef9b63253ad407890207e1",
+    "url": "./static/media/KnightWhite.7982c32a.svg"
+  },
+  {
+    "revision": "d9209c2a972f04c28ddb4cd93e0c5d18",
+    "url": "./static/media/Orbit.d9209c2a.png"
+  },
+  {
+    "revision": "369998fac7ec723ace2f39ea2a5c0c17",
+    "url": "./static/media/PawnBlack.369998fa.svg"
+  },
+  {
+    "revision": "dddf5101d9038f311308aa0b56af4aff",
+    "url": "./static/media/PawnWhite.dddf5101.svg"
+  },
+  {
+    "revision": "e2511d6adfb51b336ca4885614367759",
+    "url": "./static/media/QueenBlack.e2511d6a.svg"
+  },
+  {
+    "revision": "93b3efd01ad9b3ef5f96d6a6c0fccf0c",
+    "url": "./static/media/QueenWhite.93b3efd0.svg"
+  },
+  {
+    "revision": "96a67f947db52690ec25d84c1c42ec7d",
+    "url": "./static/media/RookBlack.96a67f94.svg"
+  },
+  {
+    "revision": "f578ee144264c56507e35fa81a2c778e",
+    "url": "./static/media/RookWhite.f578ee14.svg"
+  },
+  {
+    "revision": "52a835255e8a2074c696c55af43d26ec",
+    "url": "./static/media/Sberbank.52a83525.svg"
+  },
+  {
+    "revision": "279c14df651a2a2f0df1dd0f98627e90",
+    "url": "./static/media/bg1.279c14df.png"
+  },
+  {
+    "revision": "b8cbfea1001faf15a0e24a04db0bf604",
+    "url": "./static/media/book.b8cbfea1.png"
+  },
+  {
+    "revision": "b47eb663a8c271c4c0433c1227e37e99",
+    "url": "./static/media/boyEmoji.b47eb663.png"
+  },
+  {
+    "revision": "e5872595bba9774b635321e670727a4c",
+    "url": "./static/media/facebook-square-brands.e5872595.svg"
+  },
+  {
+    "revision": "cc5e92b0767b99437ee32d191c9c2342",
+    "url": "./static/media/instagram-brands.cc5e92b0.svg"
+  },
+  {
+    "revision": "bfe2275cd4b6ce4d5ffedbc4ffd772de",
+    "url": "./static/media/mc_vrt_pos 1.bfe2275c.svg"
+  },
+  {
+    "revision": "889a88fa2b49121aea40e5ee8cf231c8",
+    "url": "./static/media/quality.889a88fa.png"
+  },
+  {
+    "revision": "113459200cadd841dbe7680deaa684a0",
+    "url": "./static/media/rocket.11345920.png"
+  },
+  {
+    "revision": "12d7507bd8bca77a5f15461b2e4dbbb3",
+    "url": "./static/media/telegram-plane-brands.12d7507b.svg"
+  },
+  {
+    "revision": "e86b218c75e67c73166a0c8dd9163f25",
+    "url": "./static/media/twitter-brands.e86b218c.svg"
+  },
+  {
+    "revision": "b2046f674a30da7ceeba6a0072a0ab75",
+    "url": "./static/media/visa 1.b2046f67.svg"
+  },
+  {
+    "revision": "7ec09de5d0990ad2b99f1e4bd24fca83",
+    "url": "./static/media/vk-brands.7ec09de5.svg"
+  },
+  {
+    "revision": "8b656c8b1ce54f115dbc0e547a11cecc",
+    "url": "./static/media/youtube-brands.8b656c8b.svg"
+  }
+]);
